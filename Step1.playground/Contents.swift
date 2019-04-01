6ca99a2ef6eb06661f504d1929eae655029bf702
cmindy <minjidev@naver.com>
2019-04-01 16:27:14 +0900

    1-2. 2,3단 계산과 출력

--- Step1.playground/Contents.swift
+++ Step1.playground/Contents.swift
@@ -3,3 +3,25 @@ import UIKit
 var str = "Hello, iOS World"
 
 print(str)
+
+print(2*1)
+print(2*2)
+print(2*3)
+print(2*4)
+print(2*5)
+print(2*6)
+print(2*7)
+print(2*8)
+print(2*9)
+
+print(3*1)
+print(3*2)
+print(3*3)
+print(3*4)
+print(3*5)
+print(3*6)
+print(3*7)
+print(3*8)
+print(3*9)
+
+
